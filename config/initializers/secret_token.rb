5c21f204e092592f665a089d9021bc6918648e68
Kate Flood <katefloodster@gmail.com>
2014-05-11 13:29:57 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bikewithus::Application.config.secret_key_base = 'eb14422fc1dfa307c4bbd941497f4bd31e3a31343537cab7369d06eba0a8f54bc9bc80a5995255302ed54a8d8076e66bf691d04636141dc2ede403feea846c63'
